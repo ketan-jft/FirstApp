a213296bc205cf30d0d8a842a8a2faec854c07a6
Ketan <ketan.pandey@jellyfishtechnologies.com>
2017-06-30 11:40:03 +0530

    Second Commit

---
+++ grails-app/domain/com/firstApp/User.groovy
@@ -0,0 +1,16 @@
+
+  package com.firstApp 
+
+  class User 
+
+  { 
+
+     String username; 
+
+     String password; 
+
+     String email;
+
+     static constraints = { email email: true; } 
+
+  } 
